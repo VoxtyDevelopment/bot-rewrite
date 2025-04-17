df1f90e287befab3f9909d45147a8c2060a1eb76
voxty <135042947+voxty@users.noreply.github.com>
2025-04-17 06:31:13 -0400

    Update utilites.ts

--- src/utilites/utilites.ts
+++ src/utilites/utilites.ts
@@ -1,6 +1,19 @@
 import { TeamSpeak } from "ts3-nodejs-library";
+import { google } from 'googleapis';
+import { JWT } from 'google-auth-library';
 import config from "../config";
 import mysql from 'mysql2';
+
+const TEMPLATE_ID = '1Ur4T8yG3dDi_p9RAW3h8QqwPy7zbCTzeleXIvLhcwL4';
+
+export const auth = new JWT({
+    email: config.google.email,
+    key: config.google.privatekey!.replace(/\\n/g, '\n'),
+    scopes: [
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/documents',
+    ],
+})
 
 const con = mysql.createPool({
     connectionLimit: 100, 
@@ -20,4 +33,52 @@ const ts3 = new TeamSpeak({
     nickname: config.ts3.nickname,
 });
 
+export async function generateDocument(name: string): Promise<string> {
+    const copyResponse = await drive.files.copy({
+      fileId: TEMPLATE_ID,
+      requestBody: {
+        name: `${name} | DAR Report`,
+      },
+    });
+  
+    const newDocId = copyResponse.data.id!;
+    const docLink = `https://docs.google.com/document/d/${newDocId}/edit`;
+  
+    await docs.documents.batchUpdate({
+      documentId: newDocId,
+      requestBody: {
+        requests: [
+          {
+            replaceAllText: {
+              containsText: { text: '{{name}}', matchCase: true },
+              replaceText: name,
+            },
+          },
+        ],
+      },
+    });
+  
+    await drive.permissions.create({
+      fileId: newDocId,
+      requestBody: {
+        role: 'reader',
+        type: 'anyone',
+      },
+    });
+  
+    await drive.permissions.create({
+      fileId: newDocId,
+      requestBody: {
+        role: 'writer',
+        type: 'group',
+        emailAddress: 'eastcoastrp@googlegroups.com',
+      },
+      sendNotificationEmail: false,
+    });
+  
+    return docLink;
+}
+
+export const drive = google.drive({ version: 'v3', auth });
+export const docs = google.docs({ version: 'v1', auth });
 export default { con, ts3 };
