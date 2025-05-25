9bd29520c1930727fa0ae5d33421fcfcf4d3ae19
mhm <149430954+ebt-mhm@users.noreply.github.com>
2025-05-25 05:15:14 -0400

    github didnt port this over ig???

--- src/utilites/utilites.ts
+++ src/utilites/utilites.ts
@@ -4,7 +4,9 @@ import mysql from 'mysql2';
 import axios from 'axios';
 import https from 'https';
 import { EmbedBuilder, Client, ColorResolvable } from 'discord.js';
-const headers = { 'User-Agent': 'ECRP_Bot/2.0'};
+import { client } from "../index";
+
+const headers = { 'User-Agent': 'ECRP_Bot/2.0' };
 
 const httpsAgent = new https.Agent({
     rejectUnauthorized: false
@@ -16,8 +18,7 @@ const con = mysql.createPool({
     user: config.mysql.user,
     password: config.mysql.password,
     database: config.mysql.database,
-    port: 3306,
-    multipleStatements: true
+    port: 3306
 });
 
 const ts3 = new TeamSpeak({
@@ -29,53 +30,54 @@ const ts3 = new TeamSpeak({
     nickname: config.ts3.nickname,
 });
 
-export async function changeWebsiteRole(webid: string, roleid: string) {
+// logs to discord reallly should be done in a better way but this is fine for now
+/**
+ * @param title 
+ * @param fields array of fields for the embed 
+ */
+export async function logToDiscord(title: string, fields: { name: string, value: string, inline?: boolean }[]) {
   try {
-    await axios.post(
-      `https://${config.invision.domain}/api/core/members/${webid}?group=${roleid}&key=${config.invision.api}`,
-      {},
-      { headers, httpsAgent }
-  );
-  } catch (error) {
-    return console.error(error);
+    const logChannel = client.channels.cache.get(config.channels.logs);
+    if (!logChannel || !logChannel.isTextBased()) return;
+
+    const embed = new EmbedBuilder()
+      .setTitle(title)
+      .setColor(config.bot.settings.embedcolor as ColorResolvable)
+      .setImage(config.server.logo)
+      .setTimestamp()
+      .setFooter({ text: config.bot.settings.embedfooter, iconURL: config.server.logo })
+      .addFields(fields);
+
+    if (logChannel && "send" in logChannel) await logChannel.send({ embeds: [embed] });
+  } catch (err) {
+    console.error("Failed to log to Discord:", err);
   }
+}
+
+export async function changeWebsiteRole(webid: string, roleid: string) {
+    try {
+        await axios.post(
+            `https://${config.invision.domain}/api/core/members/${webid}?group=${roleid}&key=${config.invision.api}`,
+            {},
+            { headers, httpsAgent }
+        );
+    } catch (error) {
+        return console.error(error);
+    }
 };
 
-/**
- * logs event to discord!!!
- * @param client Discord.js Client instance
- * @param title Title of the embed
- * @param fields Array of fields for the embed
- */
-export async function logToDiscord(client: Client, title: string, fields: { name: string, value: string, inline?: boolean }[]) {
+export async function banWebsiteUser(webid: string) {
     try {
-        const logChannel = client.channels.cache.get(config.channels.logs);
-        if (!logChannel || !logChannel.isTextBased()) return;
-
-        const embed = new EmbedBuilder()
-            .setTitle(title)
-            .setColor(config.bot.settings.embedcolor as ColorResolvable)
-            .setImage(config.server.logo)
-            .setTimestamp()
-            .setFooter({ text: config.bot.settings.embedfooter, iconURL: config.server.logo })
-            .addFields(fields);
-
-            if (logChannel && 'send' in logChannel) await logChannel.send({ embeds: [embed] });
-    } catch (err) {
-        console.error("Failed to log to Discord:", err);
+        await axios.post(
+            `https://${config.invision.domain}/api/core/members/${webid}/warnings?suspendPermanent&moderator=1&points=100&key=${config.invision.api}`,
+            {},
+            { headers, httpsAgent }
+        );
+    } catch (error) {
+        return console.error(error);
     }
 }
 
-export async function banWebsiteUser(webid: string) {
-  try {
-    await axios.post(
-      `https://${config.invision.domain}/api/core/members/${webid}/warnings?suspendPermanent&moderator=1&points=100&key=${config.invision.api}`,
-      {},
-      { headers, httpsAgent }
-  );
-  } catch (error) {
-    return console.error(error);
-  }
-}
-
-export default { con, ts3, logToDiscord };
+export default {
+    con, ts3, logToDiscord
+};
